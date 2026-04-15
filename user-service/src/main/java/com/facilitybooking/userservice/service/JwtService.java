a17803c40a92d2c6c0416fa99e159d173fa4f959
JingPen-g <22301658@studentmail.ul.ie>
2026-04-15 08:17:16 +0100

    Update JwtService

--- user-service/src/main/java/com/facilitybooking/userservice/service/JwtService.java
+++ user-service/src/main/java/com/facilitybooking/userservice/service/JwtService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
@@ -59,7 +60,6 @@ public class JwtService {
     }
 
     private SecretKey getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 }
